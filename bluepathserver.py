218ecdc63320c7e7238c302c09bcf9664bc86bf8
Cam <59908731+scriche@users.noreply.github.com>
2024-11-06 22:49:21 -0800

    Fix Server POST

--- bluepathserver.py
+++ bluepathserver.py
@@ -21,19 +21,20 @@ def receive_log():
 
 def udp_server(server_ip, server_port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind((server_ip, server_port))
     print(f"Server listening on {server_ip}:{server_port}")
 
     while True:
-        data, addr = sock.recvfrom(1024)
+        data, addr = sock.recvfrom(2048)
         log = data.decode()
         ip = addr[0]
         print(f"Received log from {ip}: {log}")
-        requests.post('http://127.0.0.1:5000/logs', json={'ip': ip, 'log': log})
+        requests.post('http://127.0.0.1:8080/logs', json={'ip': ip, 'log': log})
 
 if __name__ == "__main__":
     server_ip = '0.0.0.0'
     server_port = int(input("Enter the server port: "))
 
     threading.Thread(target=udp_server, args=(server_ip, server_port)).start()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
