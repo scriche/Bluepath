4cc3abf0f9d21c23a69d95022d9a09e0456cee59
Cam <59908731+scriche@users.noreply.github.com>
2024-11-20 02:02:02 -0800

    Update bluepathserver.py

--- bluepathserver.py
+++ bluepathserver.py
@@ -16,7 +16,18 @@ def receive_log():
     data = request.get_json()
     ip = data['ip']
     log = data['log']
+<<<<<<< Updated upstream
     log_data[ip] = [log]  # Replace old data with new data
+=======
+    # split the log data by newline and store it in the log_data dictionary
+    # then store the data as 4 values separated by a comma
+    for line in log.split('\n'):
+        log_data[ip].append(line)
+        if len(log_data[ip]) > 4:
+            log_data[ip].pop(0)
+    # for 
+
+>>>>>>> Stashed changes
     return 'Log received', 200
 
 def udp_server(server_ip, server_port):
