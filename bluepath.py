84b3611fd57e06867e118ab5d31faa7747bb779e
scriche <59908731+scriche@users.noreply.github.com>
2025-02-07 13:50:34 -0800

    Finished clients

--- bluepath.py
+++ bluepath.py
@@ -10,36 +10,46 @@ log_file = Path('./bluepath.log')
 # Clear the log file at the start of the program
 log_file.open('w').close()
 
-def write_to_log(address, rssi, name):
-    """Write device, rssi values, and name to a log file"""
+def write_to_log(devices):
+    """Write all devices, rssi values, and names to a log file"""
     now = datetime.now()
     current_time = now.strftime('%H:%M:%S')
-    with log_file.open('a') as dev_log:
-        dev_log.write(f'{current_time},{name},{address},{rssi}\n')
-
-def remove_from_log(address):
-    """Remove device entry from the log file"""
-    if log_file.exists():
-        with log_file.open('r') as dev_log:
-            lines = dev_log.readlines()
-        with log_file.open('w') as dev_log:
-            for line in lines:
-                if address not in line:
-                    dev_log.write(line)
+    devices_to_remove = []
+    with log_file.open('w') as dev_log:
+        for addr, device in list(devices.items()):
+            try:
+                device_properties = device.GetAll('org.bluez.Device1')
+                rssi = device_properties.get('RSSI')
+                name = device_properties.get('Name', 'Unknown')
+                dev_log.write(f'{current_time},{name},{addr},{rssi}\n')
+            except Exception as e:
+                if 'org.freedesktop.DBus.Error.UnknownObject' in str(e):
+                    devices_to_remove.append(addr)
+                    print(f'Device out of range {addr}')
+                else:
+                    print(f'Error writing to log for device {addr}: {e}')
+    for addr in devices_to_remove:
+        del devices[addr]
 
 def update_devices(devices):
-    """Scan devices dictionary for devices and update the log file with the 
-    new RSSI values and remove devices that are out of range"""
-    log_file.open('w').close()
-    for addr, device in devices.items():
-        device_properties = device.GetAll('org.bluez.Device1')
-        rssi = device_properties.get('RSSI')
-        name = device_properties.get('Name', 'Unknown')
-        if rssi:
-            write_to_log(addr, rssi, name)
-        else:
-            del devices[addr]
-            print(f'Device out of range {name} ({addr})')
+    """Update the dictionary with the new RSSI values and remove devices that are out of range"""
+    to_remove = []
+    for addr, device in list(devices.items()):
+        try:
+            device_properties = device.GetAll('org.bluez.Device1')
+            rssi = device_properties.get('RSSI')
+            if rssi is None:
+                to_remove.append(addr)
+                print(f'Device out of range {device_properties.get("Name", "Unknown")} ({addr})')
+        except Exception as e:
+            if 'org.freedesktop.DBus.Error.UnknownObject' in str(e):
+                to_remove.append(addr)
+                print(f'Device out of range {addr}')
+            else:
+                print(f'Error updating device {addr}: {e}')
+    for addr in to_remove:
+        del devices[addr]
+
 bus = pydbus.SystemBus()
 mainloop = GLib.MainLoop()
 devices = {}
@@ -54,9 +64,10 @@ def discovery(path_obj):
     rssi = device_properties.get('RSSI')
     name = device_properties.get('Name', 'Unknown')
     if rssi:
-        print(f'Device added to monitor {name} ({device.Address}) @ {rssi} dBm')
-        # Add device to devices dictionary
-        devices[device.Address] = device
+        if device.Address not in devices:
+            print(f'Device added to monitor {name} ({device.Address}) @ {rssi} dBm')
+            # Add device to devices dictionary
+            devices[device.Address] = device
 
 def new_iface(path, iface_props):
     device_addr = iface_props.get('org.bluez.Device1', {}).get('Address')
@@ -66,22 +77,29 @@ def new_iface(path, iface_props):
 def start_updater():
     threading.Timer(3.0, start_updater).start()
     update_devices(devices)
+    write_to_log(devices)
     
 # Bluez object manager
-mngr = bus.get('org.bluez', '/')
-mngr.onInterfacesAdded = new_iface
+try:
+    mngr = bus.get('org.bluez', '/')
+    mngr.onInterfacesAdded = new_iface
 
-adapter = bus.get('org.bluez', '/org/bluez/hci0')
-adapter.DuplicateData = False
+    adapter = bus.get('org.bluez', '/org/bluez/hci0')
+    adapter.DuplicateData = False
 
-adapter.StartDiscovery()
-print('Finding devices...')
+    adapter.StartDiscovery()
+    print('Finding devices...')
+
+    start_updater()
+except Exception as e:
+    print(f'Error initializing Bluez: {e}')
 
 try:
-    start_updater()
     mainloop.run()
 except KeyboardInterrupt:
     # Stop discovery and exit the program
     adapter.StopDiscovery()
     mainloop.quit()
+    # Clear the log file
+    log_file.open('w').close()
     print('Exiting...')
--- bluepathclient.py
+++ bluepathclient.py
@@ -5,18 +5,21 @@ def send_log_file(server_ip, server_port, log_file_path):
     with open(log_file_path, 'r') as file:
         log_data = file.read()
     
+    sock = None
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.sendto(log_data.encode(), (server_ip, server_port))
     except socket.error as e:
         print(f"Error sending log file: {e}")
     finally:
-        sock.close()
+        if sock:
+            sock.close()
 
 def main():
     server_ip = input("Enter the server IP address: ")
     server_port = int(input("Enter the server port: "))
     log_file_path = "./bluepath.log"
+    print(f"Sending log file to {server_ip}:{server_port}...")
 
     while True:
         send_log_file(server_ip, server_port, log_file_path)
